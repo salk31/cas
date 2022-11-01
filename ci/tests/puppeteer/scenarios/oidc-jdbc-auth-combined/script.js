0c9864f20b1a7efbbed2130635163a2c57e8ecb7
Sam Hough <samuel.hough@diamond.ac.uk>
2022-11-01 12:14:42 +0000

    parse error: Expected separator between values at line 3, column 14
    doh. script.json

--- ci/tests/puppeteer/scenarios/oidc-jdbc-auth-combined/script.js
+++ ci/tests/puppeteer/scenarios/oidc-jdbc-auth-combined/script.js
@@ -1,6 +1,6 @@
 const puppeteer = require('puppeteer');
 const cas = require('../../cas.js');
-const assertx = require('assert');
+const assert = require('assert');
 
 (async () => {
     const browser = await puppeteer.launch(cas.browserOptions());
